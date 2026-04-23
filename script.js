2414f640e9b13d9555a0310a0a78380d3474c6d8
Harshit Shukla <harshitshukla3655@gmail.com>
2026-04-24 01:01:17 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+ function startLearning() {
+    alert("🚀 Welcome! Let's start your Game Dev journey!");
+  }
+
+  function enrollNow() {
+    alert("🎯 You are enrolled! We will contact you soon.");
+  }
+
+  function featureClick(feature) {
+    alert("You clicked on: " + feature);
+  }
